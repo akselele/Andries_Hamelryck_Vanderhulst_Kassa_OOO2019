952969a35ba43c5a128f71826b9ecc4524fe6653
akselele <axelelexd@gmail.com>
2019-11-28 14:12:35 +0100

    Story 1 - Facade (context) toegevoegd en werkt. Ook @Author tags toegevoegd.

--- src/database/ArtikelDbContext.java
+++ src/database/ArtikelDbContext.java
@@ -1,5 +1,44 @@
 package database;
 
-public class ArtikelDbContext  {
+/**
+@Author Axel Hamelryck
+**/
 
+import com.sun.javafx.collections.MappingChange;
+import javafx.collections.ObservableList;
+import model.Artikel;
+import model.DomainException;
+
+import java.util.Map;
+
+public class ArtikelDbContext {
+    private ArtikelDbStrategy artikelDbStrategy;
+
+    public ArtikelDbContext() throws DomainException {
+        artikelDbStrategy = new ArtikelDbInMemory();
+    }
+
+    public Artikel getArtikel(String code){
+        return artikelDbStrategy.get(code);
+    }
+
+    public ObservableList<Artikel> getAll(){
+        return artikelDbStrategy.getAll();
+    }
+
+    public Map<String, Artikel> getArtikels(){
+        return artikelDbStrategy.getArtikels();
+    }
+
+    public void load() throws DomainException{
+        artikelDbStrategy.load();
+    }
+
+    public void add(Artikel artikel){
+        artikelDbStrategy.add(artikel);
+    }
+
+    public void save() throws DomainException{
+        artikelDbStrategy.save();
+    }
 }
